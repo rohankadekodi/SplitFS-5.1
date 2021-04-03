b193d13c3eb1e9a7ba121f8b90d7da6e13938749
Chameleon Cloud User <cc@duofs-node-2.novalocal>
2021-04-03 16:28:04 +0000

    printing free hugepages

--- fs/duofs/pmfs_stats.c
+++ fs/duofs/pmfs_stats.c
@@ -57,6 +57,7 @@ void pmfs_print_available_hugepages(struct super_block *sb)
 	struct pmfs_sb_info *sbi = PMFS_SB(sb);
 	int i;
 	unsigned long num_hugepages = 0;
+	unsigned long num_free_blocks = 0;
 	struct free_list *free_list;
 
 
@@ -64,12 +65,15 @@ void pmfs_print_available_hugepages(struct super_block *sb)
 	for (i = 0; i < sbi->cpus; i++) {
 		free_list = pmfs_get_free_list(sb, i);
 		num_hugepages += free_list->num_blocknode_huge_aligned;
+		/*
 		printk("free list idx %d, free hugepages %lu, free unaligned pages %lu\n",
 		       free_list->index, free_list->num_blocknode_huge_aligned,
 		       free_list->num_blocknode_unaligned);
+		*/
+		num_free_blocks += free_list->num_free_blocks;
 	}
-	printk("Total free hugepages %lu\n",
-	       num_hugepages);
+	printk("Total free hugepages %lu, Total free blocks = %lu, Possible free hugepages = %lu\n",
+	       num_hugepages, num_free_blocks, num_free_blocks / 512);
 }
 
 void pmfs_print_timing_stats(void)
